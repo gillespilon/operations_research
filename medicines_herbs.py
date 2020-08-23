b2654758b7352915b4c3d217c6a9933cd631e2f0
Gilles Pilon <gillespilon13@gmail.com>
2020-08-23 15:18:31 -0500

    Make code more explicit

--- medicines_herbs.py
+++ medicines_herbs.py
@@ -5,20 +5,32 @@ Example of linear programming using PuLP.
 '''
 
 
-from pulp import *
+from pulp import LpProblem, LpMaximize, LpVariable, LpInteger
 
 
 # Create the linear programming problem object
-problem = LpProblem('miracle_worker', LpMaximize)
+problem = LpProblem(name='medicines_and_herbs', sense=LpMaximize)
 # Create the linear programming variable objects
-x = LpVariable('medicine_1_units', 0, None, LpInteger)
-y = LpVariable('medicine_2_units', 0, None, LpInteger)
+x = LpVariable(
+    name='medicine_1_units',
+    lowBound=0,
+    upBound=None,
+    cat=LpInteger
+)
+y = LpVariable(
+    'medicine_2_units',
+    0,
+    None,
+    LpInteger
+)
 # Add the objective function
 problem += 25*x + 20*y, 'health restored, to be maximized'
 # Add the constraints
 problem += 3*x + 4*y <= 25, 'herb A constraint'
 problem += 2*x + y <= 10, 'herb B constraint'
 # Write the problem data to a .lp file
-problem.writeLP('miracleworker.lp')
+problem.writeLP(
+    'medicines_herbs.lp'
+)
 # Solve the problem using PuLP's choice of solver
 problem.solve()

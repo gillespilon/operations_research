33824a49311fe3a56f48be09840ce9c94d1b0ccf
Gilles Pilon <gillespilon13@gmail.com>
2020-08-23 21:48:29 -0500

    Make code more explicit

--- plants_warehouses.py
+++ plants_warehouses.py
@@ -5,7 +5,9 @@ Example of linear programming using PuLP.
 '''
 
 
-from pulp import *
+from pulp import (
+    LpProblem, LpMinimize, LpVariable, LpInteger, utilities, lpSum, LpStatus
+)
 
 
 # Create list of plants
@@ -29,27 +31,39 @@ costs = [
     [14, 8, 11]
 ]
 # Make costs into dictionary
-costs = makeDict([plants, warehouses], costs, 0)
+costs = utilities.makeDict(
+    headers=[plants, warehouses], array=costs, default=0
+)
 # Create the linear programming problem object
-problem = LpProblem('plant_warehouse_problem', LpMinimize)
+problem = LpProblem(name='plant_warehouse_problem', sense=LpMinimize)
 # Create list of tuples containing all lanes
 routes = [(plant, warehouse) for plant in plants for warehouse in warehouses]
 # Create dictionary containing all lanes
-vars = LpVariable.dicts('route', (plants, warehouses), 0, None, LpInteger)
+vars = LpVariable.dicts(
+    name='route',
+    indexs=(plants, warehouses),
+    lowBound=0,
+    upBound=None,
+    cat=LpInteger
+)
 # Add the objective function
-problem += lpSum([vars[plant][warehouse]*costs[plant][warehouse] for (plant, warehouse) in routes])
+problem += lpSum([vars[plant][warehouse]*costs[plant][warehouse]
+                 for (plant, warehouse) in routes])
 # Add supply maximum constraints to problem for each supply note (plant)
 for plant in plants:
-    problem += lpSum([vars[plant][warehouse] for warehouse in warehouses]) <= supply[plant], 'sum_of_products_out_of_plnats_%s'%plant
+    problem += lpSum([vars[plant][warehouse] for warehouse in warehouses])\
+            <= supply[plant], 'sum_of_products_out_of_plnats_%s' % plant
 for warehouse in warehouses:
-    problem += lpSum([vars[plant][warehouse] for plant in plants]) >= demand[warehouse], 'sum_of_products_into_warehouses%s'%warehouse
-problem.writeLP('plants_warehouses.lp')
+    problem += lpSum([vars[plant][warehouse] for plant in plants])\
+            >= demand[warehouse], 'sum_of_products_into_warehouses%s'\
+            % warehouse
+problem.writeLP(filename='plants_warehouses.lp')
 # Solve the problem using PuLP's choice of solver
-problem.solve()
+problem.solve(solver=None)
 # Print status of solution
 print('Status', LpStatus[problem.status])
 # Print each variable with it' resolved optimum value
 for v in problem.variables():
     print(v.name, '=', v.varValue)
 # Print the optimized objective function
-print('Total cost of transportation =', value(problem.objective))
+print('Total cost of transportation =', utilities.value(problem.objective))
